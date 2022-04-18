18260ecaeeb8eea9df608f6ff05fe4a6e28935a0
coldricedev <mastercoldrice@gmail.com>
2022-04-17 17:12:25 -0700

    Added title screen
    added title screen

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class StartManager : MonoBehaviour
+{
+    public TMP_InputField textInput;
+
+    public TMP_Text errorText;
+ 
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+
+    bool TextInputValid(string txt)
+    {
+        if(txt != null && txt != "" && txt != " ")
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public void StartGame()
+    {
+        Debug.Log("clicked!");
+        string text = textInput.text.Trim();
+
+        if (TextInputValid(text))
+        {
+            //save here
+
+            SceneManager.LoadScene(1);
+        }else
+        {
+            errorText.gameObject.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textInput != null)
+        {
+            Debug.Log(textInput.text);
+        }
+    }
+}
